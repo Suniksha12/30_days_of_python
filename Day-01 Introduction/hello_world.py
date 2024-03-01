c3aa7f73116f14a7498f09516c8d6e119e51042a
Suniksha <sunikshabenpatel@gmail.com>
2024-03-01 09:59:11 +0530

    Day-01

---
+++ Day-01 Introduction/hello_world.py
@@ -0,0 +1,24 @@
+#Introduction towards python (single line comment)
+
+#Welcome to our
+#30 days of Python 
+# challenge (multiline comment '''  ''' or ### (each new line))
+
+print(2 + 3)     #addition(+)
+print(3 - 1)     #subtraction(-)
+print(2 * 4)     #multiplication(*)
+print(4 / 3)     #division(/)
+print(3 ** 2)    #exponential(**)
+print(3 % 2)     #modulus(%)
+print(3 // 2)    #Floor division operator(//)
+
+#checking for data types
+
+print(type(10))                              #int
+print(type(3.14))                            #float
+print(type(1 + 3j))                          #complex
+print(type('Suniksha'))                      #string
+print(type([1,2,3]))                         #list
+print(type({'name' : 'suniksha','age': 20})) #Dictionary
+print(type({9.8,3.14,2.7}))                  #set
+print(type((9.8,3.14,2.7)))                  #Tuple
