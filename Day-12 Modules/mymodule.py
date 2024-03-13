bb6acec8eafc1bc156e53b80e682f7cc5b149cdc
Suniksha <sunikshabenpatel@gmail.com>
2024-03-13 14:42:41 +0530

    Day-12

---
+++ Day-12 Modules/mymodule.py
@@ -0,0 +1,14 @@
+def generate_full_name(firstname, lastname):
+      space = ' '
+      fullname = firstname + space + lastname
+      return fullname
+
+def sum_two_nums (num1, num2):
+    return num1 + num2
+gravity = 9.81
+person = {
+    "firstname": "Suniksha",
+    "age": 20,
+    "country": "Finland",
+    "city":'Helsinki'
+}
